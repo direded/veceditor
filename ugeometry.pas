9d2eca9e5fad91d6b49e8e51bd90289f50b7b1ce
Direded <dmpalyonniy@gmail.com>
2016-12-04 21:17:09 +1000

    Fix logic in function IsPointInLineSegment

--- ugeometry.pas
+++ ugeometry.pas
@@ -13,8 +13,9 @@ implementation
 
 function IsPointInLineSegment(A, B, P: TDoublePoint; Eps: Double = 0): Boolean;
 begin
-  if (abs(GetVecMultiplyLength(P-A, B-A)) <= Eps)  and
-      (Min(A.X, B.X) <= P.X) and (P.X <= Max(A.X, B.X)) then Exit(true);
+  if (abs(GetVecMultiplyLength(P-A, B-A)) <= Eps) and
+    (Min(A.X, B.X) <= P.X) and (P.X <= Max(A.X, B.X)) and
+    (Min(A.Y, B.Y) <= P.Y) and (P.Y <= Max(A.Y, B.Y)) then Exit(true);
   Exit(false);
 end;
 
